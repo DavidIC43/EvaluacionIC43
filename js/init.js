af73b0a507a32ce6a1c04d44004e237a06b56cca
DavidIC43 <81993719+DavidIC43@users.noreply.github.com>
2021-04-13 23:12:01 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyDxzqfDwcu3Cg-VbQw7Fs0LlU-G77KjFjc",
-    authDomain: "bdalum43.firebaseapp.com",
-    projectId: "bdalum43",
-    storageBucket: "bdalum43.appspot.com",
-    messagingSenderId: "626826242814",
-    appId: "1:626826242814:web:2592f2586fa2a14c9d53af",
-    measurementId: "G-61LJKL0SJD"
+   apiKey: "AIzaSyB97DJ5cIN6jSt-EFZmT1fau_acLPLrn2I",
+    authDomain: "bddavidic43.firebaseapp.com",
+    projectId: "bddavidic43",
+    storageBucket: "bddavidic43.appspot.com",
+    messagingSenderId: "556439627302",
+    appId: "1:556439627302:web:40ab4eff3af11419dc68e7",
+    measurementId: "G-WGGSM4FX91
 });
